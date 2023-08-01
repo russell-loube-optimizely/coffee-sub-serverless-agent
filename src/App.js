6f721d6d169001ddefc523b34d6af59c7f82d203
russell-loube-optimizely <russell.loube@optimizely.com>
2023-08-01 10:54:33 -0500

    opti sdk instrumented but not configured properly

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ import Grind from "./components/Grind.js";
 
 import React, { useState, useEffect } from "react";
 
-import Cookies from "js-cookie";
-
 function App() {
   const [deliveryValue, setDeliveryValue] = useState("1");
   const [addedToCart, setAddedToCart] = useState(false);
@@ -41,36 +39,66 @@ function App() {
     setAddedToCart(updatedAddedToCart);
   };
 
-  return (
-    <div className="container">
-      <FogLightNavbar />
-      <br />
-      <Row>
-        <ImageCard />
-        <Col>
-          <Card>
-            <Card.Body>
-              <Card.Title>Coffee Subscription</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">$18.00</Card.Subtitle>
-              <br />
-              <Grind grind={grind} />
-              <br />
-              <Type type={type} />
-              <br />
-              <br />
-              <Card.Text>Delivery Frequency</Card.Text>
-              <DeliveryFrequency />
-              <br />
-              <AddToCartButton
-                addedToCart={addedToCart}
-                handleClick={handleCartClick}
-              />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
+  const optimizely = require("@optimizely/optimizely-sdk");
+  const optimizelyClient = optimizely.createInstance({
+    sdkKey: "BNFZhjLJcatSku1y6XLAM",
+  });
+
+  const [datafileFetched, setDatafileFetched] = useState(false);
+  const [isClientReady, setIsClientReady] = useState(false);
+
+  useEffect(() => {
+    optimizelyClient.onReady().then(() => {
+      setDatafileFetched(true);
+      if (isClientValid) {
+        setIsClientReady(true);
+      }
+    }, []);
+  });
+
+  const isClientValid = () => {
+    return optimizelyClient.getOptimizelyConfig() !== null;
+  };
+
+  const user = optimizelyClient.createUserContext("user123");
+  const decision = user.decide("product_detail_page");
+  const title = decision.variables.title;
+  console.log(decision);
+
+  if (isClientReady === true) {
+    return (
+      <div className="container">
+        <FogLightNavbar />
+        <br />
+        <Row>
+          <ImageCard />
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>Coffee Subscription</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  $18.00
+                </Card.Subtitle>
+                <br />
+                <Grind grind={grind} />
+                <br />
+                <Type type={type} />
+                <br />
+                <br />
+                <Card.Text>Delivery Frequency</Card.Text>
+                <DeliveryFrequency />
+                <br />
+                <AddToCartButton
+                  addedToCart={addedToCart}
+                  handleClick={handleCartClick}
+                />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default App;
